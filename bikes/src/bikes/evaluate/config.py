65a528577e812170d1d9e8b72f332fcd74738ed0
Lukas Malms <lukas.malms@gmail.com>
2025-06-24 09:14:41 +0100

    Move study notebooks into separate folder

--- bikes/src/bikes/evaluate/config.py
+++ bikes/src/bikes/evaluate/config.py
@@ -16,6 +16,30 @@ VALIDATION_PERIODS = [
     (datetime(2024, 12, 1), datetime(2025, 1, 1)),
 ]
 
+TEST_PERIOD = datetime(2024, 10, 1), datetime(2025, 1, 1)
+
+TEST_LOCATIONS = [
+    "Dominion Road",
+    "Upper Queen Street",
+    "Grafton Road",
+    "Orewa Path",
+    "Lake Road",
+    "Beach Road",
+    "Mangere Bridge",
+    "Westhaven Drive",
+    "Grafton Gully",
+    "Waterview Unitec",
+    "Grafton Bridge",
+    "Karangahape Road",
+    "Nelson Street",
+    "NW Cycleway TeAtatu",
+    "Lightpath",
+    "Quay Street - Spark Arena",
+    "NW Cycleway Kingsland",
+    "Tamaki Drive",
+    "Quay Street Eco Display Classic",
+]
+
 
 @dataclass
 class PipelineConfig:
