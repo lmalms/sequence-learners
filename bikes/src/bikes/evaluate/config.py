92aa842a6577ef89924f99fdbb69ccff870508c5
Lukas Malms <lukas.malms@gmail.com>
2025-07-10 00:36:22 +0100

    WIP evaluate and compare forecasts

--- bikes/src/bikes/evaluate/config.py
+++ bikes/src/bikes/evaluate/config.py
@@ -1,20 +1,4 @@
-from dataclasses import dataclass
 from datetime import datetime
-from typing import Any
-
-from bikes.models.ets import ExponentialSmoothingLearner
-from bikes.models.prophet import ProphetSequenceLearner
-from bikes.models.rnn import RNNSequenceLearner
-from bikes.models.sarima import SARIMASequenceLearner
-from bikes.models.theta import ThetaSequenceLearner
-
-VALIDATION_PERIODS = [
-    (datetime(2024, 8, 1), datetime(2024, 9, 1)),
-    (datetime(2024, 9, 1), datetime(2024, 10, 1)),
-    (datetime(2024, 10, 1), datetime(2024, 11, 1)),
-    (datetime(2024, 11, 1), datetime(2024, 12, 1)),
-    (datetime(2024, 12, 1), datetime(2025, 1, 1)),
-]
 
 TEST_PERIOD = datetime(2024, 10, 1), datetime(2025, 1, 1)
 
@@ -39,43 +23,3 @@ TEST_LOCATIONS = [
     "Tamaki Drive",
     "Quay Street Eco Display Classic",
 ]
-
-
-@dataclass
-class PipelineConfig:
-    learner_class: Any
-    learner_kwargs: dict[str, Any]
-    transformer_class: Any = None
-    transformer_kwargs: dict[str, Any] | None = None
-
-
-LOCATION_MODEL_CONFIGS: dict[str, list[PipelineConfig]] = {
-    "Nelson Street": [
-        PipelineConfig(
-            learner_class=ExponentialSmoothingLearner,
-            learner_kwargs={
-                "trend": "add",
-                "damped_trend": True,
-                "seasonal": "mul",
-                "seasonal_periods": 7,
-                "freq": "D",
-            },
-        ),
-        PipelineConfig(
-            learner_class=SARIMASequenceLearner,
-            learner_kwargs={
-                "order": (1, 1, 2),
-                "seasonal_order": (1, 1, 1, 7),
-                "freq": "D",
-            },
-        ),
-        PipelineConfig(
-            learner_class=ThetaSequenceLearner,
-            learner_kwargs={},
-        ),
-        PipelineConfig(
-            learner_class=ProphetSequenceLearner,
-            learner_kwargs={},
-        ),
-    ],
-}
--- bikes/src/bikes/evaluate/metrics.py
+++ bikes/src/bikes/evaluate/metrics.py
@@ -2,15 +2,25 @@ import numpy as np
 import pandas as pd
 
 
-def mase(y_train: pd.Series, y_true: pd.Series, y_hat: pd.Series, season: int = 1):
+def mase(y_train: pd.Series, y_true: pd.Series, y_pred: pd.Series, season: int = 1):
     """
     Mean Absolute Scaled Error
     """
 
     y_train = np.array(y_train)
     y_true = np.array(y_true)
-    y_hat = np.array(y_hat)
+    y_pred = np.array(y_pred)
 
-    forecast_error = np.abs(y_true - y_hat)
+    forecast_error = np.abs(y_true - y_pred)
     in_sample_error = np.abs(y_train[season:] - y_train[:-season])
     return np.mean(forecast_error) / np.mean(in_sample_error)
+
+
+def mape(y_true: pd.Series, y_pred: pd.Series):
+    """
+    Mean Absolute Percentage Error
+    """
+
+    y_true = np.array(y_true)
+    y_pred = np.array(y_pred)
+    return np.abs((y_true - y_pred) / y_true)
