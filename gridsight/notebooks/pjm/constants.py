9049d9379550ff4cddcba5b2c43dfca6ae8cea92
Lukas Malms <lukas.malms@gmail.com>
2026-01-11 23:26:55 +0000

    Refine validation constants

--- gridsight/notebooks/pjm/constants.py
+++ gridsight/notebooks/pjm/constants.py
@@ -15,16 +15,16 @@ PJM_SITE_NAMES = [
 ]
 
 DEFAULT_VALIDATION_WINDOWS = [
-    (datetime(2017, 5, 10, 12), datetime(2017, 5, 12, 12)),
-    (datetime(2017, 6, 8, 10), datetime(2017, 6, 10, 10)),
-    (datetime(2017, 7, 14, 14), datetime(2017, 7, 16, 14)),
-    (datetime(2017, 8, 15, 3), datetime(2017, 8, 17, 3)),
-    (datetime(2017, 9, 21, 7), datetime(2017, 9, 23, 7)),
-    (datetime(2017, 10, 23, 18), datetime(2017, 10, 25, 18)),
-    (datetime(2017, 11, 7, 23), datetime(2017, 11, 9, 23)),
-    (datetime(2017, 12, 19, 2), datetime(2017, 12, 21, 2)),
-    (datetime(2018, 1, 28, 12), datetime(2018, 1, 30, 12)),
-    (datetime(2018, 2, 14, 19), datetime(2018, 2, 16, 19)),
+    (datetime(2017, 5, 10, 12), datetime(2017, 5, 12, 12)),  # [Wednesday, Friday]
+    (datetime(2017, 6, 8, 10), datetime(2017, 6, 10, 10)),  # [Thursday, Saturday]
+    (datetime(2017, 7, 14, 14), datetime(2017, 7, 16, 14)),  # [Friday, Sunday]
+    (datetime(2017, 8, 12, 3), datetime(2017, 8, 14, 3)),  # [Saturday, Monday]
+    (datetime(2017, 9, 24, 7), datetime(2017, 9, 26, 7)),  # [Sunday, Tuesday]
+    (datetime(2017, 10, 23, 18), datetime(2017, 10, 25, 18)),  # [Monday, Wednesday]
+    (datetime(2017, 11, 7, 23), datetime(2017, 11, 9, 23)),  # [Tuesday, Thursday]
+    (datetime(2017, 12, 20, 2), datetime(2017, 12, 22, 2)),  # [Wednesday, Friday]
+    (datetime(2018, 1, 25, 12), datetime(2018, 1, 27, 12)),  # [Thursday, Saturday]
+    (datetime(2018, 2, 16, 19), datetime(2018, 2, 18, 19)),  # [Friday, Sunday]
 ]
 VALIDATION_WINDOWS = {
     "AEP": DEFAULT_VALIDATION_WINDOWS,
@@ -36,16 +36,16 @@ VALIDATION_WINDOWS = {
     "EKPC": DEFAULT_VALIDATION_WINDOWS,
     "FE": DEFAULT_VALIDATION_WINDOWS,
     "NI": [
-        (datetime(2010, 3, 10, 12), datetime(2010, 3, 12, 12)),
-        (datetime(2010, 4, 8, 10), datetime(2010, 4, 10, 10)),
-        (datetime(2010, 5, 14, 14), datetime(2010, 5, 16, 14)),
-        (datetime(2010, 6, 15, 3), datetime(2010, 6, 17, 3)),
-        (datetime(2010, 7, 21, 7), datetime(2010, 7, 23, 7)),
-        (datetime(2010, 8, 23, 18), datetime(2010, 8, 25, 18)),
-        (datetime(2010, 9, 7, 23), datetime(2010, 9, 9, 23)),
-        (datetime(2010, 10, 19, 2), datetime(2010, 10, 21, 2)),
-        (datetime(2010, 11, 28, 12), datetime(2010, 11, 30, 12)),
-        (datetime(2010, 12, 14, 19), datetime(2010, 12, 16, 19)),
+        (datetime(2010, 3, 10, 12), datetime(2010, 3, 12, 12)),  # [Wednesday, Friday]
+        (datetime(2010, 4, 8, 10), datetime(2010, 4, 10, 10)),  # [Thursday, Saturday]
+        (datetime(2010, 5, 7, 14), datetime(2010, 5, 9, 14)),  # [Friday, Sunday]
+        (datetime(2010, 6, 12, 3), datetime(2010, 6, 14, 3)),  # [Saturday, Monday]
+        (datetime(2010, 7, 18, 7), datetime(2010, 7, 20, 7)),  # [Sunday, Tuesday]
+        (datetime(2010, 8, 23, 18), datetime(2010, 8, 25, 18)),  # [Monday, Wednesday]
+        (datetime(2010, 9, 7, 23), datetime(2010, 9, 9, 23)),  # [Tuesday, Thursday]
+        (datetime(2010, 10, 13, 2), datetime(2010, 10, 15, 2)),  # [Wednesday, Friday]
+        (datetime(2010, 11, 18, 12), datetime(2010, 11, 20, 12)),  # [Thursday, Saturday]
+        (datetime(2010, 12, 17, 19), datetime(2010, 12, 19, 19)),  # [Friday, Sunday]
     ],
     "PJME": DEFAULT_VALIDATION_WINDOWS,
     "PJMW": DEFAULT_VALIDATION_WINDOWS,
